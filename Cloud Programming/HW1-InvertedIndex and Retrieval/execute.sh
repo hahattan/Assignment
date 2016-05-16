5461371110f8c301d7d11c1377f9fc701321a74c
Chung-Yuan Hung <j406660003@gmail.com>
2016-05-16 14:08:38 +0800

    Move CP to another repo

--- Cloud Programming/HW1-InvertedIndex and Retrieval/execute.sh
+++
@@ -1,6 +0,0 @@
-hdfs dfs -rm -r /user/s101062115/InvertedIndex
-hdfs dfs -rm -r /user/s101062115/Query
-
-hadoop jar InvertedIndex.jar invertedIndex.InvertedIndex input /user/s101062115/InvertedIndex
-hadoop jar Query.jar query.Query /user/s101062115/InvertedIndex input /user/s101062115/Query
-hdfs dfs -cat /user/s101062115/Query/part-*
--- Cloud Programming/Lab5-WordCount/execute.sh
+++
@@ -1,9 +0,0 @@
-# Do not uncomment these lines to directly execute the script
-# Modify the path to fit your need before using this script
-#hdfs dfs -rm -r /user/TA/WordCount/Output/
-#hadoop jar WordCount.jar wordcount.WordCount /user/shared/WordCount/Input /user/TA/WordCount/Output
-#hdfs dfs -cat /user/TA/WordCount/Output/part-*
-
-hdfs dfs -rm -r /user/s101062115/WordCount/output/
-hadoop jar WordCount.jar wordcount.WordCount /user/s101062115/input1 /user/s101062115/WordCount/output
-hdfs dfs -cat /user/s101062115/WordCount/output/part-*
